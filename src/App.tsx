fe1cba72f7a69e76477e8f8b366a35f69cc2e9f5
Hosh'ki Tsunoda <hoshkitsunoda@gmail.com>
2020-12-24 00:36:02 -0800

    created TwoColumn layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
 import { FC } from 'react'
 
+import TwoColumnLayout from './components/Layout/TwoColumnLayout'
+
+/*
+create a component to set props and pass to each child component
+ie. Setting component sets {state} which holds all settings and pass to Layout
+*/
+
 const App: FC = () => {
-  return <div className="App">APP</div>
+  return (
+    <>
+      <TwoColumnLayout
+        className="main"
+        columnLeftWidth="200"
+        backgroundLeft="yellow"
+        backgroundRight="lime"
+        leftContent={<LeftContent />}
+        rightContent={<RightContent />}
+        boxShadow={true}
+        fullHeight={true}
+      />
+    </>
+  )
 }
+
+const LeftContent = () => (
+  <>
+    <header>My Blog</header>
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#archive">Archive</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </>
+)
+const RightContent = () => (
+  <>
+    {[1, 2, 3].map((item) => (
+      <div key={item} style={{ height: '200px', backgroundColor: 'tomato', margin: '1rem' }}>
+        <h1>Content {item}</h1>
+      </div>
+    ))}
+  </>
+)
 
 export default App
---
+++ src/components/Layout/TwoColumnLayout/index.tsx
@@ -0,0 +1,77 @@
+import { ReactNode, FC } from 'react'
+import styled from 'styled-components'
+
+interface Props {
+  className?: string
+  columnLeftWidth: string
+  backgroundLeft?: string
+  backgroundRight?: string
+  leftContent?: ReactNode
+  rightContent?: ReactNode
+  boxShadow?: boolean
+  fullHeight?: boolean
+}
+
+const TwoColumnLayout: FC<Props> = ({
+  className,
+  columnLeftWidth,
+  backgroundLeft,
+  backgroundRight,
+  leftContent,
+  rightContent,
+  boxShadow,
+  fullHeight,
+  ...props
+}: Props) => {
+  return (
+    <>
+      <Wrapper
+        className={className}
+        columnLeftWidth={columnLeftWidth}
+        backgroundLeft={backgroundLeft}
+        backgroundRight={backgroundRight}
+        leftContent={leftContent}
+        rightContent={rightContent}
+        boxShadow={boxShadow}
+        fullHeight={fullHeight}
+        {...props}
+      />
+    </>
+  )
+}
+
+const Wrapper = styled(({ className, leftContent, rightContent }: Props) => {
+  return (
+    <div className={className}>
+      <>
+        <LeftColumn content={leftContent} />
+      </>
+      <>
+        <RightColumn content={rightContent} />
+      </>
+    </div>
+  )
+})`
+  display: flex;
+  height: ${({ fullHeight }) => (fullHeight ? '100vh' : null)};
+
+  & > div:first-child {
+    flex: ${({ columnLeftWidth }) => columnLeftWidth}px 0 0;
+    background: ${({ backgroundLeft }) => backgroundLeft};
+    box-shadow: ${({ boxShadow }) =>
+      boxShadow ? '10px -6px 25px rgba(0,0,0,0.16), -6px -3px 26px rgba(0,0,0,0.23)' : null};
+    padding: 10px;
+    z-index: 1;
+  }
+
+  & > div:last-child {
+    flex: auto 1 0;
+    background: ${({ backgroundRight }) => backgroundRight};
+    padding: 10px;
+  }
+`
+
+const LeftColumn = ({ content }: { content: ReactNode }) => <div className="left">{content}</div>
+const RightColumn = ({ content }: { content: ReactNode }) => <div className="right">{content}</div>
+
+export default TwoColumnLayout
